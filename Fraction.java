7cd14164b2ab597801a906248a11e63a4818c2fb
deancasbay <144666784+deancasbay@users.noreply.github.com>
2023-09-25 14:08:08 -0400

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,5 +1,4 @@
-public class Fraction implements INumber<Fraction>
-{
+public class Fraction implements INumber<Fraction>, Comparable<Fraction> {
     protected double numerator, denominator;
     public Fraction(final double numerator, final double denominator) {
         this.numerator = numerator;
@@ -30,4 +29,64 @@ public class Fraction implements INumber<Fraction>
     public void print() {
         System.out.printf("%f/%f\n", numerator, denominator);
     }
+    
+    public Fraction findMax(Fraction[] input) {
+        Fraction max = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].compareTo(max) == 1) {
+                max = input[i];
+            }
+        }
+        return max;
+    }
+
+    public Fraction findMin(Fraction[] input) {
+        Fraction min = input[0];
+        for (int i = 1; i < input.length; i++) {
+            if (input[i].compareTo(min) == -1) {
+                min = input[i];
+            }
+        }
+        return min;
+    }
+    
+    
+    public int getCountGreater(Fraction[] inputArray, Fraction inputFraction) {
+        int count = 0;
+        for (Fraction fraction : inputArray) {
+            if (fraction.compareTo(inputFraction) == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    public int getCountGreater(Fraction[] inputArray, Fraction inputFraction, int currentIndex) {
+        if (currentIndex >= inputArray.length) {
+            return 0;
+        }
+
+        int compareResult = inputArray[currentIndex].compareTo(inputFraction);
+        int count = 0;
+
+        if (compareResult == 1) {
+            count = 1;
+        }
+
+        return count + getCountGreater(inputArray, inputFraction, currentIndex + 1);
+    }
+    
+    public int compareTo(Fraction other) {
+        // Compare fractions based on their values
+        double thisValue = this.numerator / this.denominator;
+        double otherValue = other.numerator / other.denominator;
+        if (thisValue < otherValue) {
+            return -1;
+        } else if (thisValue > otherValue) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
 }
